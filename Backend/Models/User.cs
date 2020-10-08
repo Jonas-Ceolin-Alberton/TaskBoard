9656c2baf786f50db042014e8a5c3daf3d71b388
JONAS-CEOLIN-ALBERTON <jonasceolin@gmail.com>
2020-10-08 17:30:04 -0300

    fix

---
+++ Backend/Models/User.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Backend.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
